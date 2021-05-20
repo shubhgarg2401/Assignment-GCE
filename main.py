fb389b9b756bf31b59c1f1b728533cba46106e09
Shubh Garg <53488130+shubhgarg2401@users.noreply.github.com>
2021-05-21 00:57:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import requests
+import json
+import numpy
 
+r=requests
+
+
+
+
+
+
+if r.status_code == 200:
+    a.sort()
+    b.sort()
+    c=a+b
+elif response.status_code == 404:
+    print('Not Found.')
