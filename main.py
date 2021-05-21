71d60b289df2787636c95382fdfbc8b6cab0d406
Shubh Garg <53488130+shubhgarg2401@users.noreply.github.com>
2021-05-21 20:00:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ r=requests
 
 
 
-if r.status_code == 200:
-    a.sort()
-    b.sort()
-    c=a+b
-elif response.status_code == 404:
-    print('Not Found.')
+# initializing lists
+test_list1 = [1, 5, 6, 9, 11]
+test_list2 = [3, 4, 7, 8, 10]
+  
+# using sorted()
+# to combine two sorted lists
+res = sorted(test_list1 + test_list2)
+  
+# printing result
+print ("The combined sorted list is : " + str(res))
