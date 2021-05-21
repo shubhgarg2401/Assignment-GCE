d4341dfc281babb0af76f4e53861f819c948bdc8
Shubh Garg <53488130+shubhgarg2401@users.noreply.github.com>
2021-05-22 00:54:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import requests
-import json
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
 
-
-
-r=requests
-
-
-# initializing lists
-test_list1 = [1, 5, 6, 9, 11]
-test_list2 = [3, 4, 7, 8, 10]
+    test_list1 = a from input
+    test_list2 = b from input 
   
-# using sorted()
-# to combine two sorted lists
-res = sorted(test_list1 + test_list2)
+
+    res = sorted(test_list1 + test_list2)
   
-# printing result
-print ("The combined sorted list is : " + str(res))
+
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return f'Okay'
+    elif request_json and 'message' in request_json:
+        return str(res)
+    else:
+        return f'Hello World!'
