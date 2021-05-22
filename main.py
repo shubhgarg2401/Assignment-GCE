6ce9a42cc63ccfac484c6efd1251d72270a22a92
Shubh Garg <53488130+shubhgarg2401@users.noreply.github.com>
2021-05-22 15:08:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def hello_world(request):
+import json
+
+#json library comes pre-installed with python, so no need to include it in the requirments.txt file
+
+def assignment_gce(request):
+    
+    #Creating a function by the name of assignment_gce which will alos be used as an entry point to the function.
+    
     """Responds to any HTTP request.
     Args:
         request (flask.Request): HTTP request object.
@@ -7,18 +14,25 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-
-    test_list1 = a from input
-    test_list2 = b from input 
+    
+    
+    data1='message1' in request.get_json()      #storing the json payload dictonary
+    data2='message2' in request.get_json()
+    
+    list1=json.loads(data1)
+    list2=json.loads(data2)
+    
+    
+     
   
 
-    res = sorted(test_list1 + test_list2)
+    res = sorted(list1 + list2)
   
 
     request_json = request.get_json()
     if request.args and 'message' in request.args:
-        return f'Okay'
+        return f'Try again using the JSON payload'
     elif request_json and 'message' in request_json:
         return str(res)
     else:
-        return f'Hello World!'
+        return f'Invalid Input, Try Again!'
